fc65c3259e6c3bc7a054acdd4a25e5b7335b17e1
brad marder <bradmarder@gmail.com>
2025-11-08 09:48:45 -0500

    fix scoping oopsie

--- server/LogMiddleware.cs
+++ server/LogMiddleware.cs
@@ -3,38 +3,48 @@
 public class LogMiddleware(
 	ILogger<LogMiddleware> _logger,
 	LootGodContext _db,
-	LootService _service) : IMiddleware
+	LootService _lootService) : IMiddleware
 {
 	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
 	{
 		using var _ = _logger.BeginScope(new
 		{
-			IP = _service.GetIPAddress(),
+			IP = _lootService.GetIPAddress(),
 		});
 
-		if (context.Request.Method is not ("POST" or "DELETE") || _service.GetPlayerKey() is null)
+		if (context.Request.Method is not ("POST" or "DELETE") || _lootService.GetPlayerKey() is null)
 		{
 			await next(context);
 			return;
 		}
 
+		var player = TryGetPlayer();
+		using var __ = _logger.BeginScope(new
+		{
+			PlayerId = player?.Id,
+			PlayerName = player?.Name,
+			GuildId = player?.GuildId,
+			GuildName = player?.Guild.Name,
+		});
+
+		await next(context);
+	}
+
+	private Player? TryGetPlayer()
+	{
 		try
 		{
-			var playerId = _service.GetPlayerId();
-			var player = _db.Players
+			var playerId = _lootService.GetPlayerId();
+
+			return _db.Players
 				.AsNoTracking()
 				.Include(x => x.Guild)
 				.Single(x => x.Id == playerId);
-			using var __ = _logger.BeginScope(new
-			{
-				PlayerId = playerId,
-				PlayerName = player.Name,
-				GuildId = player.GuildId,
-				GuildName = player.Guild.Name,
-			});
 		}
-		catch { } // no need to log if any error is thrown - the pipeline will throw the same error later to be caught by the GlobalExceptionHandler
-
-		await next(context);
+		catch
+		{
+			// no need to log if any error is thrown - the pipeline will throw the same error later to be caught by the GlobalExceptionHandler
+			return null;
+		}
 	}
 }
