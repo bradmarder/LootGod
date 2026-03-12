8b78e41398f42d2811c1ed0f1270159b5358ff6c
brad marder <bradmarder@gmail.com>
2026-03-11 22:10:01 -0400

    cleanup logs

--- server/Log.cs
+++ server/Log.cs
@@ -1,92 +1,92 @@
 ﻿public static partial class Log
 {
-	[LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Spell sync {SpellCount} of {TotalSpellCount} in {ElapsedMs}")]
+	[LoggerMessage(1, LogLevel.Information, "Spell sync {SpellCount} of {TotalSpellCount} in {ElapsedMs}")]
 	public static partial void SpellSyncSuccess(this ILogger logger, int spellCount, int totalSpellCount, long elapsedMs);
 
-	[LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "Item sync {RaidItemCount} of {TotalItemCount} in {ElapsedMs}")]
+	[LoggerMessage(2, LogLevel.Information, "Item sync {RaidItemCount} of {TotalItemCount} in {ElapsedMs}")]
 	public static partial void ItemSyncSuccess(this ILogger logger, int raidItemCount, int totalItemCount, long elapsedMs);
 
-	[LoggerMessage(EventId = 3, Level = LogLevel.Information, Message = "Loot requests finished")]
+	[LoggerMessage(3, LogLevel.Information, "Loot requests finished")]
 	public static partial void LootRequestsFinished(this ILogger logger);
 
-	[LoggerMessage(EventId = 4, Level = LogLevel.Information, Message = "DataSink created")]
+	[LoggerMessage(4, LogLevel.Information, "DataSink created")]
 	public static partial void DataSinkCreated(this ILogger logger);
 
-	[LoggerMessage(EventId = 5, Level = LogLevel.Information, Message = "DataSink removed after {Elapsed}s")]
+	[LoggerMessage(5, LogLevel.Information, "DataSink removed after {Elapsed}s")]
 	public static partial void DataSinkRemoved(this ILogger logger, int elapsed);
 
-	[LoggerMessage(EventId = 6, Level = LogLevel.Information, Message = "Loot request created")]
+	[LoggerMessage(6, LogLevel.Information, "Loot request created")]
 	public static partial void LootRequestCreated(this ILogger logger);
 
-	[LoggerMessage(EventId = 7, Level = LogLevel.Information, Message = "Loot request deleted")]
+	[LoggerMessage(7, LogLevel.Information, "Loot request deleted")]
 	public static partial void LootRequestDeleted(this ILogger logger);
 
-	[LoggerMessage(EventId = 8, Level = LogLevel.Information, Message = "Loot request granted - {LootRequestId} - {Granted}")]
+	[LoggerMessage(8, LogLevel.Information, "Loot request granted - {LootRequestId} - {Granted}")]
 	public static partial void LootRequestGranted(this ILogger logger, int lootRequestId, bool granted);
 
-	[LoggerMessage(EventId = 9, Level = LogLevel.Error, Message = "Discord read content failed")]
+	[LoggerMessage(9, LogLevel.Error, "Discord read content failed")]
 	public static partial void DiscordReadContentError(this ILogger logger, Exception ex);
 
-	[LoggerMessage(EventId = 10, Level = LogLevel.Warning, Message = "Import dump warning - `{Message}`")]
+	[LoggerMessage(10, LogLevel.Warning, "Import dump warning - `{Message}`")]
 	public static partial void ImportDumpWarning(this ILogger logger, string message);
 
-	[LoggerMessage(EventId = 11, Level = LogLevel.Error, Message = "GlobalExceptionHandler - {RequestPath}")]
+	[LoggerMessage(11, LogLevel.Error, "GlobalExceptionHandler - {RequestPath}")]
 	public static partial void GlobalExceptionHandler(this ILogger logger, Exception ex, string requestPath);
 
-	[LoggerMessage(EventId = 12, Level = LogLevel.Information, Message = "Raid dump import completed")]
+	[LoggerMessage(12, LogLevel.Information, "Raid dump import completed")]
 	public static partial void RaidDumpImportCompleted(this ILogger logger);
 
-	[LoggerMessage(EventId = 13, Level = LogLevel.Information, Message = "Guild dump import completed")]
+	[LoggerMessage(13, LogLevel.Information, "Guild dump import completed")]
 	public static partial void GuildDumpImportCompleted(this ILogger logger);
 
-	[LoggerMessage(EventId = 14, Level = LogLevel.Information, Message = "Discord webhook success")]
+	[LoggerMessage(14, LogLevel.Information, "Discord webhook success")]
 	public static partial void DiscordWebhookSuccess(this ILogger logger);
 
-	[LoggerMessage(EventId = 15, Level = LogLevel.Information, Message = "Database backup created `{TempFileName}` at {Now} in {Elapsed}ms")]
+	[LoggerMessage(15, LogLevel.Information, "Database backup created `{TempFileName}` at {Now} in {Elapsed}ms")]
 	public static partial void DatabaseBackup(this ILogger logger, string tempFileName, long now, long elapsed);
 
-	[LoggerMessage(EventId = 16, Level = LogLevel.Information, Message = "Payload delivery completed in {Elapsed}ms")]
+	[LoggerMessage(16, LogLevel.Information, "Payload delivery completed in {Elapsed}ms")]
 	public static partial void PayloadDeliveryComplete(this ILogger logger, long elapsed);
 
-	[LoggerMessage(EventId = 17, Level = LogLevel.Information, Message = "Guild created")]
+	[LoggerMessage(17, LogLevel.Information, "Guild created")]
 	public static partial void GuildCreated(this ILogger logger);
 
-	[LoggerMessage(EventId = 18, Level = LogLevel.Information, Message = "Loot (un)locked - {Enabled}")]
+	[LoggerMessage(18, LogLevel.Information, "Loot (un)locked - {Enabled}")]
 	public static partial void LootLocked(this ILogger logger, bool enabled);
 
-	[LoggerMessage(EventId = 19, Level = LogLevel.Information, Message = "Alt linked - {AltName}")]
+	[LoggerMessage(19, LogLevel.Information, "Alt linked - {AltName}")]
 	public static partial void AltLinked(this ILogger logger, string altName);
 
-	[LoggerMessage(EventId = 20, Level = LogLevel.Information, Message = "Alt unlinked - {AltName}")]
+	[LoggerMessage(20, LogLevel.Information, "Alt unlinked - {AltName}")]
 	public static partial void AltUnlinked(this ILogger logger, string altName);
 
-	[LoggerMessage(EventId = 21, Level = LogLevel.Information, Message = "Guild leadership transferred from `{OldGuildLeaderName}` to `{NewGuildLeaderName}`")]
+	[LoggerMessage(21, LogLevel.Information, "Guild leadership transferred from `{OldGuildLeaderName}` to `{NewGuildLeaderName}`")]
 	public static partial void GuildLeadershipTransferred(this ILogger logger, string oldGuildLeaderName, string newGuildLeaderName);
 
-	[LoggerMessage(EventId = 22, Level = LogLevel.Error, Message = "Broken connection detected - {ConnectionId}")]
+	[LoggerMessage(22, LogLevel.Error, "Broken connection detected - {ConnectionId}")]
 	public static partial void BrokenConnection(this ILogger logger, Exception ex, string connectionId);
 
-	[LoggerMessage(EventId = 23, Level = LogLevel.Information, Message = "Loot quantity updated")]
+	[LoggerMessage(23, LogLevel.Information, "Loot quantity updated")]
 	public static partial void LootQuantityUpdated(this ILogger logger);
 
-	[LoggerMessage(EventId = 24, Level = LogLevel.Error, Message = "Discord Webhook Failure")]
+	[LoggerMessage(24, LogLevel.Error, "Discord Webhook Failure")]
 	public static partial void DiscordWebhookFailure(this ILogger logger, Exception ex);
 
-	[LoggerMessage(EventId = 25, Level = LogLevel.Information, Message = "Database vacuum success in {Elapsed}ms")]
+	[LoggerMessage(25, LogLevel.Information, "Database vacuum success in {Elapsed}ms")]
 	public static partial void DatabaseVacuumSuccess(this ILogger logger, long elapsed);
 
-	[LoggerMessage(EventId = 26, Level = LogLevel.Information, Message = "Application started")]
+	[LoggerMessage(26, LogLevel.Information, "Application started")]
 	public static partial void ApplicationStarted(this ILogger logger);
 
-	[LoggerMessage(EventId = 27, Level = LogLevel.Information, Message = "Application stopping - {CancellationRequested}")]
+	[LoggerMessage(27, LogLevel.Information, "Application stopping - {CancellationRequested}")]
 	public static partial void ApplicationStopping(this ILogger logger, bool cancellationRequested);
 
-	[LoggerMessage(EventId = 28, Level = LogLevel.Information, Message = "Application stopped - {CancellationRequested}")]
+	[LoggerMessage(28, LogLevel.Information, "Application stopped - {CancellationRequested}")]
 	public static partial void ApplicationStopped(this ILogger logger, bool cancellationRequested);
 
-	[LoggerMessage(EventId = 29, Level = LogLevel.Information, Message = "Required player key missing")]
+	[LoggerMessage(29, LogLevel.Information, "Required player key missing")]
 	public static partial void RequiredPlayerKeyMissing(this ILogger logger);
 
-	[LoggerMessage(EventId = 30, Level = LogLevel.Information, Message = "Removed player trying to log in")]
+	[LoggerMessage(30, LogLevel.Information, "Removed player trying to log in")]
 	public static partial void RemovedPlayerLogin(this ILogger logger);
 }
